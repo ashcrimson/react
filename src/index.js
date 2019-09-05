e3c1bddf31d3c89cf527a4fe90d4046c654de3c3
Felipe Pino <felipepinoredes@gmail.com>
2019-09-05 17:26:37 -0400

    Add files via upload
    Tutorial react.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './mysass.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class MyhHeader extends React.Component {
+	render() {
+		return (
+			<div>
+			<h1>Hello Style!</h1>
+			<p>Add a little style!</p>
+			</div>
+			);
+	}
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(<MyhHeader />, document.getElementById('root'));
+
